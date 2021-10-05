2de210a8e9de0ac2650bfc60fd6d2a60a72e41f3
Sergej Sakac <sakacszergej@gmail.com>
2021-10-05 15:19:22 +0200

    commit

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -32,47 +32,12 @@ namespace ShopApp
         public MainPage()
         {
             this.InitializeComponent();
-            this.Loaded += LoadedHandler;
+            frame.Navigate(typeof(Products));
         }
-        private async void LoadedHandler(object sender, RoutedEventArgs e)
-		{
-            await getProducts();
-		}
-        async public Task getProducts()
-		{
-            string url = "http://alcoin-shop.herokuapp.com/api/posts?category=null&page=0";
-            try
-            {
-                //We will now define your HttpClient with your first using statement which will use a IDisposable.
-                using (HttpClient client = new HttpClient())
-                {
-                    using (HttpResponseMessage res = await client.GetAsync(url)) 
-                    {
-                        using (HttpContent content = res.Content)
-                        {
-                            var data = await content.ReadAsStringAsync();
-							Console.WriteLine(data);
 
-                            if(data != null)
-							{
-                                var dataArr = JArray.Parse(data);
-                                for(int i = 0; i < dataArr.Count; i++)
-								{
-                                    var dataObj = dataArr[i];
-                                    Product prod = new Product($"{dataObj["title"]}",
-                                        $"{dataObj["price"]}", $"{dataObj["image"]}");
-                                    this.Products.Add(prod);
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-            catch (Exception exception)
-            {
-                Console.WriteLine("Exception Hit------------");
-                Console.WriteLine(exception);
-            }
+		private void Add_Prod(object sender, RoutedEventArgs e)
+		{
+            frame.Navigate(typeof(AddProd));
 		}
-    }
+	}
 }
---
+++ Products.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace ShopApp
+{
+	/// <summary>
+	/// An empty page that can be used on its own or navigated to within a Frame.
+	/// </summary>
+	public sealed partial class Products : Page
+	{
+        public ObservableCollection<Product> ProductsList { get; }
+        = new ObservableCollection<Product>();
+        public Products()
+		{
+			this.InitializeComponent();
+            this.Loaded += LoadedHandler;
+        }
+        private async void LoadedHandler(object sender, RoutedEventArgs e)
+        {
+            await getProducts();
+        }
+        async public Task getProducts()
+        {
+            string url = "http://alcoin-shop.herokuapp.com/api/posts?category=null&page=0";
+            try
+            {
+                //We will now define your HttpClient with your first using statement which will use a IDisposable.
+                using (HttpClient client = new HttpClient())
+                {
+                    using (HttpResponseMessage res = await client.GetAsync(url))
+                    {
+                        using (HttpContent content = res.Content)
+                        {
+                            var data = await content.ReadAsStringAsync();
+                            Console.WriteLine(data);
+
+                            if (data != null)
+                            {
+                                var dataArr = JArray.Parse(data);
+                                for (int i = 0; i < dataArr.Count; i++)
+                                {
+                                    var dataObj = dataArr[i];
+                                    Product prod = new Product($"{dataObj["title"]}",
+                                        $"{dataObj["price"]}", $"{dataObj["image"]}");
+                                    this.ProductsList.Add(prod);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine("Exception Hit------------");
+                Console.WriteLine(exception);
+            }
+        }
+    }
+}
