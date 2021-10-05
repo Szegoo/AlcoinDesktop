fe11c81f3af407498b47b58b8dee52930c7810f0
Sergej Sakac <sakacszergej@gmail.com>
2021-10-05 12:05:18 +0200

    first commit

---
+++ Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ShopApp;
+
+namespace ShopApp
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public string Price { get; set; }
+        public string Image { get; set; }
+
+        public Product(string name ,string price, string image)
+		{
+            Name = name;
+            Price = price;
+            Image = image;
+		}
+    }
+}
