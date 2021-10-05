a8c9d72782a8867d60226d4bf6fae53a0c563957
Sergej Sakac <sakacszergej@gmail.com>
2021-10-05 18:21:38 +0200

    commit

--- AddProd.xaml.cs
+++ AddProd.xaml.cs
@@ -12,6 +12,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using System.Net.Http;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -22,9 +23,24 @@ namespace ShopApp
 	/// </summary>
 	public sealed partial class AddProd : Page
 	{
+		public string message = "";
 		public AddProd()
 		{
 			this.InitializeComponent();
+		}
+
+		async private void AddProduct(object sender, RoutedEventArgs e)
+		{
+			HttpClient client = new HttpClient();
+			string url = "http://alcoin-shop.herokuapp.com/api/addNavItem";
+			var values = new Dictionary<string, string>
+			{
+				{ "name", prodName.Text }
+			};
+			var content = new FormUrlEncodedContent(values);
+
+			var response = await client.PostAsync(url, content);
+			message = "Category added!";
 		}
 	}
 }
